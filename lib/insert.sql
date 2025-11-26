cec5bbcd6e923c060f49e721c7b156b3713f624f
simonkaruga <simonkaruganjoroge@gmail.com>
2025-11-26 20:46:37 +0300

    Add origin

---
+++ lib/insert.sql
@@ -0,0 +1,24 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Mr. Chocolate', 12, 'M', 'dark brown', 'calm', 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Rowdy', 8, 'M', 'brown', 'aggressive', 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Tabitha', 5, 'F', 'black', 'friendly', 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Sergeant Brown', 20, 'M', 'dark brown', 'strict', 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Melissa', 6, 'F', 'brown', 'mild', 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Grinch', 3, 'M', 'greenish', 'grumpy', 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Wendy', 4, 'F', 'white', 'sweet', 1);
+
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES (NULL, 7, 'M', 'brown', 'unknown', 1);
--- lib/sql_queries.py
+++ lib/sql_queries.py
@@ -1,18 +1,67 @@
 select_all_female_bears_return_name_and_age = """
-    Write your SQL query here
+SELECT name, age
+FROM bears
+WHERE sex='F';
 """
 
 select_all_bears_names_and_orders_in_alphabetical_order = """
-    Write your SQL query here
+SELECT name FROM bears ORDER BY name ASC;
 """
 
 select_all_bears_names_and_ages_that_are_alive_and_order_youngest_to_oldest = """
-    Write your SQL query here
+SELECT name, age FROM bears WHERE alive = 1 ORDER BY age ASC;
 """
 
 select_oldest_bear_and_returns_name_and_age = """
-    Write your SQL query here
+SELECT name, age FROM bears ORDER BY age DESC LIMIT 1;
 """
+
 select_youngest_bear_and_returns_name_and_age = """
-    Write your SQL query here
+SELECT name, age FROM bears ORDER BY age ASC LIMIT 1;
+"""
+
+select_all_bears_names_and_ages_ordered_by_age = """
+SELECT name, age
+FROM bears
+ORDER BY age;
+"""
+
+select_all_bears_names_and_ages_ordered_by_age_desc = """
+SELECT name, age
+FROM bears
+ORDER BY age DESC;
+"""
+
+select_youngest_bear = """
+SELECT *
+FROM bears
+ORDER BY age ASC
+LIMIT 1;
+"""
+
+select_oldest_bear = """
+SELECT *
+FROM bears
+ORDER BY age DESC
+LIMIT 1;
+"""
+
+select_most_aggressive_bear = """
+SELECT *
+FROM bears
+WHERE temperament='aggressive'
+LIMIT 1;
+"""
+
+select_all_bears_alive = """
+SELECT *
+FROM bears
+WHERE alive = 1;
+"""
+
+select_bears_with_name_sorted_alphabetically = """
+SELECT *
+FROM bears
+WHERE name IS NOT NULL
+ORDER BY name ASC;
 """
